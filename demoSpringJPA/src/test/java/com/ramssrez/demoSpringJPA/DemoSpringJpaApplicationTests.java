318d6c015a4a2fbd60a11ffdbab0016737073c6b
Raul Ramirez Perez <ramssrez@gmail.com>
2022-05-04 23:57:28 +0200

    Creación del proyecto para conectarse a la base de datos

---
+++ demoSpringJPA/src/test/java/com/ramssrez/demoSpringJPA/DemoSpringJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ramssrez.demoSpringJPA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
