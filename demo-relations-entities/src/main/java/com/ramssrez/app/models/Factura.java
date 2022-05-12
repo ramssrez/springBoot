f89c8b787b5f3e514bab9fe3357bc3bcdd88720e
Raul Ramirez Perez <ramssrez@gmail.com>
2022-05-12 14:40:56 +0200

    Creación de las entidades producto y factura

---
+++ demo-relations-entities/src/main/java/com/ramssrez/app/models/Factura.java
@@ -0,0 +1,25 @@
+package com.ramssrez.app.models;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "facturas")
+public class Factura {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "fecha_emision")
+    private LocalDate fechaEmisión;
+
+    @Column(name = "folio")
+    private String folio;
+
+    @Column(name = "descripcion")
+    private String descripcion;
+
+    @Column(name = "total")
+    private BigDecimal total;
+}
---
+++ demo-relations-entities/src/main/java/com/ramssrez/app/models/Producto.java
@@ -0,0 +1,19 @@
+package com.ramssrez.app.models;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "productos")
+public class Producto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "nombre")
+    private String nombre;
+
+    @Column(name = "precio")
+    private BigDecimal precio;
+
+}
