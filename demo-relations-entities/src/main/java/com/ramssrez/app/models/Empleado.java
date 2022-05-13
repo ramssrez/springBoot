386357d1a1320472e5e7038c1805c661df199305
Raul Ramirez Perez <ramssrez@gmail.com>
2022-05-13 11:52:20 +0200

    Creación de la llave compuesta para empleados

--- demo-relations-entities/src/main/java/com/ramssrez/app/models/Empleado.java
+++ demo-relations-entities/src/main/java/com/ramssrez/app/models/Empleado.java
@@ -1,6 +1,18 @@
 package com.ramssrez.app.models;
 
+import javax.persistence.Column;
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.Table;
 import java.io.Serializable;
+import java.time.LocalDate;
 
+@Entity
+@Table(name = "empleados")
 public class Empleado implements Serializable {
+    @EmbeddedId
+    private EmpleadoPK empleadoPK;
+
+    @Column(name = "fecha_ingreso", nullable = false)
+    private LocalDate fechaIngreso;
 }
--- demo-relations-entities/src/main/java/com/ramssrez/app/models/EmpleadoPK.java
+++ demo-relations-entities/src/main/java/com/ramssrez/app/models/EmpleadoPK.java
@@ -1,6 +1,18 @@
 package com.ramssrez.app.models;
 
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
 import java.io.Serializable;
-
+@Embeddable
 public class EmpleadoPK implements Serializable {
+
+    @Column(name = "codigo_empleado", unique = true, nullable = false, length = 10)
+    private String codigoEmpleado;
+
+    @Column(name = "nss", unique = true, nullable = false, length = 11)
+    private String numeroSocial;
+
+    @Column(name = "rfc", unique = true, nullable = false, length = 13)
+    private String rfc;
+
 }
